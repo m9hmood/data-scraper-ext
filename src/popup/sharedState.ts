e266abcb75d3d36f9159cee267cf7cd61cf2ea74
Mahmood <mahmood3xq@gmail.com>
2024-02-19 18:33:35 +0300

    ref(state): Remove Pinia and refactor state management
    - Removed Pinia for state management
    - Refactored state management to use a reactive object
    
    This commit removes the Pinia dependency and refactors the state management implementation. Instead of relying on Pinia, the state is now managed using a reactive object. This change improves maintainability and reduces dependencies in the project.

--- src/popup/sharedState.ts
+++ src/popup/sharedState.ts
@@ -1,6 +1,4 @@
-import { computed, reactive, ref } from 'vue'
-
-import { defineStore } from 'pinia'
+import { reactive } from 'vue'
 
 export const state = reactive({
   isTablePresent: false,
@@ -8,20 +6,4 @@ export const state = reactive({
   currentStep: 0,
   STEP_STORAGE_KEY: 'ACTIVE_STEP',
   CURRENT_SITE_ORIGIN: '',
-})
-
-export const useExtensionState = defineStore('counter', () => {
-  const STEP_STORAGE_KEY = ref<string>('ACTIVE_STEP')
-  const CURRENT_SITE_ORIGIN = ref<string>('activeStep')
-  const isPopupWaitingForResponse = ref<boolean>(false)
-  const isTablePresent = ref<boolean>(false)
-  const currentStep = ref<number>(0)
-
-  return {
-    STEP_STORAGE_KEY,
-    CURRENT_SITE_ORIGIN,
-    isPopupWaitingForResponse,
-    isTablePresent,
-    currentStep,
-  }
 })
